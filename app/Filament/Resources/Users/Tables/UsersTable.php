36332443ee147d5e4b1fcaa0114c14ac5f73d35d
riza abdullah <seorang.riza@gmail.com>
2026-04-07 14:50:49 +0700

    first commit

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("name")->searchable(),
+                TextColumn::make("email"),
+                TextColumn::make("email_verified_at")
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
